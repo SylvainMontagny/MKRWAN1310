f23dc26b817afb46bec9c13a8e0a41ea9c541178
Sylvain Montagny <sylvain.montagny@univ-smb.fr>
2021-03-24 00:45:45 +0100

    Add downlink capabilities to change  FRAME_DELAY
    First byte

--- MKRWAN1310-OTAA/MKRWAN1310-OTAA.ino
+++ MKRWAN1310-OTAA/MKRWAN1310-OTAA.ino
@@ -5,7 +5,7 @@
 
 #define FRAME_DELAY       10000
 #define DATA_RATE         5
-#define ADAPTIVE_DR       false
+#define ADAPTIVE_DR       true
 #define CONFIRMED         false
 #define PORT              1
 #define CAYENNE_LPP       true
@@ -17,19 +17,21 @@
 /**** LoRaWAN Setup ******/
 LoRaModem modem;
 String appEui = ""; 
-String appKey = "";  
+String appKey = "";   // Sylvain
+//String appKey = "";   // Tibo
 uint8_t dataToSend[20] = {0}; // Data to send
-CayenneLPP dataToSendCayenne(160);
-int dRate = 5;
-String reponse;
+uint8_t dataReceived[20] = {0}; // Data received via Donwlink
+uint32_t frameDelay = FRAME_DELAY;
 
+CayenneLPP dataToSendCayenne(51);
+CayenneLPP dataReceivedCayenne(51);
  
 void setup() {
   int connected;
   
   /***** Serial Link Configuration ******/
   Serial.begin(115200);
-  while (!Serial);
+  //while (!Serial);
   pinMode(LED_BUILTIN, OUTPUT);
   for(uint32_t i=0;i<10;i++){
       digitalWrite(LED_BUILTIN, HIGH);
@@ -43,17 +45,17 @@ void setup() {
   Serial.println("#########     OTAA activation   ########\r\n");
 
   /****** Configure Here the First Module *****/
-  //Enter here module configuration
+  //Enter here module configuration, ie : temperature sensor
 
    /****** Configure Here the Second Module *****/
-  //Enter here module configuration
+  //Enter here module configuration, ie : Weight scale
   
   /**** LoRaWAN Configuration *****/
   if (!modem.begin(EU868)) {
     Serial.println("Failed to start module");
+    while (1);
     Serial.print("Your device EUI is: ");
     Serial.println(modem.deviceEUI());
-    while (1);
   };
   
   do{
@@ -62,10 +64,10 @@ void setup() {
   if (!connected)   Serial.println(" JOIN OTAA failed!!! Retry...");
   else              Serial.println(" JOIN procedure : SUCCESS !\r\n");
   }while(!connected);
-  modem.dataRate(dRate);
+  modem.dataRate(DATA_RATE);
   modem.setADR(1);
   modem.setPort(PORT);
-  Serial.print(" Frame will be sent every ");Serial.print((FRAME_DELAY<7000)?7000:FRAME_DELAY);Serial.println("ms\r\n");  
+  Serial.print(" Frame will be sent every ");Serial.print((frameDelay<7000)?7000:frameDelay);Serial.println("ms\r\n");  
  
 }
 
@@ -84,6 +86,9 @@ void loop() {
 
 void sendLoRa(){
   int status;
+  int i = 0;
+
+  /***** Send data *****/
   if(CONFIRMED)   Serial.print(" Uplink CONFIRMED on PORT ");
   else            Serial.print(" Uplink UNCONFIRMED on PORT ");
   Serial.println(PORT);
@@ -99,24 +104,27 @@ void sendLoRa(){
   } 
   delay(3000); // Wait for downlink
 
-  char rcv[64];
-  int i = 0;
+ 
+  /***** Receive data *****/
   if (!modem.available()) {
-    Serial.println(" No data received\r\n");
+    Serial.println(" No data received");
   }
   else {
     while (modem.available()) {
-      rcv[i++] = (char)modem.read();
+      dataReceived[i++] = (char)modem.read();
     }
-    Serial.print("Received: ");
+    Serial.print(" Received Downlink (Hexa) : ");
     for (unsigned int j = 0; j < i; j++) {
-      Serial.print(rcv[j] >> 4, HEX);
-      Serial.print(rcv[j] & 0xF, HEX);
+      Serial.print(dataReceived[j] >> 4, HEX);
+      Serial.print(dataReceived[j] & 0xF, HEX);
       Serial.print(" ");
     }  
     Serial.println();
+  // First byte is the time between 2 Uplink in seconds
+  // Second byte ... to be defined
+  // Third byte ... etc...
+    frameDelay=dataReceived[0]*1000;    // First Byte
   } 
-  
   dataToSendCayenne.reset();
 }
 
@@ -137,14 +145,14 @@ void getWeight(void){
 
 void enterLowPower(void){
   Serial.print(" Processor goes in Low Power mode during : ");
-  Serial.print(FRAME_DELAY);Serial.println("ms");
+  Serial.print(frameDelay);Serial.println("ms\r\n");
   Serial.print(" Using the Low Power features usually corrupts the Serial Link ");
-  LowPower.deepSleep(FRAME_DELAY); 
+  LowPower.deepSleep(frameDelay); 
   Serial.println(" Wake Up !!!");  
 }
 
 void wait(void){
   Serial.print(" Processor is going to wait during : ");
-  Serial.print(FRAME_DELAY);Serial.println("ms");
-  delay(FRAME_DELAY); 
+  Serial.print(frameDelay);Serial.println("ms\r\n");
+  delay(frameDelay); 
 }
